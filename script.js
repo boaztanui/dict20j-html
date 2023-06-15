616094474973add310570910b82e41e97922f6c9
boaztanui <boaztanui058@gmail.com>
2023-06-15 20:57:38 +0300

    css fix

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML = "Hello Javascript";
+
+const Age = 13;
+const lastName = "Tanui";
+
+console.log(Age);
+console.log(`Hello World!`);
+console.log("Hello " + lastName);
+console.log(`Hello ${lastName}`);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Now why did you click me! 👿";
+}
+
+document.querySelector("h1").textContent = "I changed again!";
